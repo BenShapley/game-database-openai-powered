6805e15b128f69dc10e275df6333774a6caf611b
Ben Shapley <benjamin.shapley@hogarth.com>
2024-12-02 12:40:17 +0000

    C:/Program Files/Git/search site branch & default bg

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html', question="Search...", chatbot_response="", background_image="", game="Look up a game!")
+	return render_template('index.html', question="Search...", chatbot_response="", background_image="static/default_background.png", game="Look up a game!", show_absolute_background=False)
 
-@app.route('/', methods=['POST'])
+@app.route('/search', methods=['POST'])
 def index_post():
 	user_question = request.form['req_question']
 	chatbot_response = main.ask_question(user_question)
 	get_image = main.game_screenshots(user_question)
-	return render_template('index.html', question=user_question, chatbot_response=chatbot_response, background_image=get_image, game="Your Response:")
+	return render_template('index.html', question=user_question, chatbot_response=chatbot_response, background_image=get_image, game="Your Response:", show_absolute_background=True)

22deefbd6e40284cc9bacfa79faf58ac5cdac3e7
Ben Shapley <benjamin.shapley@hogarth.com>
2024-12-03 10:42:38 +0000

    created help button w/ hover event

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html', question="Search...", chatbot_response="", background_image="static/default_background.png", game="Look up a game!", show_absolute_background=False)
+	return render_template('index.html', question="Search...", chatbot_response="", background_image="static/default_background.png", 
+						game="Look up a game!")
 
 @app.route('/', methods=['POST'])
 def index_post():
 	user_question = request.form['req_question']
 	chatbot_response,game_name = main.ask_question(user_question)
 	get_image = main.game_screenshots(game_name)
-	return render_template('search.html', question=user_question, chatbot_response=chatbot_response, background_image=get_image, game=game_name, show_absolute_background=True) 
+	return render_template('search.html', question=user_question, chatbot_response=chatbot_response, background_image=get_image, 
+						game=game_name, show_absolute_background=True) 
